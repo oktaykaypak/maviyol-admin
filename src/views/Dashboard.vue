384b06c5aedf75f9e4e527deb4e13c2cd81c3003
oktaykaypak <36843265+oktaykaypak@users.noreply.github.com>
2021-08-15 21:48:32 +0300

    dasboard table for

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,744 @@
 <template>
   <!--begin::Dashboard-->
-  <div class="row gy-5 g-xl-8">:)</div>
+  <div class="row gy-5 g-xl-8">
+    <DataTable :list="viewList"></DataTable>
+    <el-pagination
+      layout="prev, pager, next"
+      :total="100"
+      @current-change="handleCurrentChange"
+    >
+    </el-pagination>
+  </div>
+
   <!--end::Dashboard-->
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent, onMounted } from "vue";
 import { setCurrentPageTitle } from "@/core/helpers/breadcrumb";
+import DataTable from "@/components/widgets/tables/Widget13.vue";
 
 export default defineComponent({
   name: "dashboard",
   components: {
+    DataTable,
+  },
+  data() {
+    return {
+      list: [
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Approved",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "05822-FXSP",
+          country: {
+            name: "Belarus",
+            code: "BY",
+          },
+          date: {
+            value: "04/18/2021",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Agoda",
+            fields: "Houses & Hotels",
+          },
+          status: {
+            label: "In Progress",
+            color: "warning",
+          },
+          total: "$4850",
+        },
+        {
+          orderid: "4472-QREX",
+          country: {
+            name: "Phillipines",
+            code: "BH",
+          },
+          date: {
+            value: "07/23/2019",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "RoadGee",
+            fields: "Transportation",
+          },
+          status: {
+            label: "Success",
+            color: "danger",
+          },
+          total: "$8376",
+        },
+        {
+          orderid: "00347-BCLQ",
+          country: {
+            name: "Argentina",
+            code: "BR",
+          },
+          date: {
+            value: "12/21/2021",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "The Hill",
+            fields: "Insurance",
+          },
+          status: {
+            label: "Rejected",
+            color: "info",
+          },
+          total: "$9486",
+        },
+        {
+          orderid: "59486-XDER",
+          country: {
+            name: "Agoda",
+            code: "BT",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Phillipines",
+            fields: "Transportation",
+          },
+          status: {
+            label: "Approved",
+            color: "primary",
+          },
+          total: "$8476",
+        },
+      ],
+      viewList: [],
+    };
+  },
+  methods: {
+    handleCurrentChange(val) {
+      this.viewList = [];
+      const index = val * 10 - 10;
 
+      try {
+        for (let i = 0; i < 10; i++) {
+          this.viewList.push(this.list[index + i]);
+        }
+      } catch {
+        return;
+      }
+    },
   },
   setup() {
     onMounted(() => {
