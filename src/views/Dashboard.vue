e0474e1c68c67b6c3a0fd45b7f6bc884e4f9d46b
Şafak ÇEBİN <safakcebin@hotmail.com>
2021-09-01 20:56:45 +0300

    asd

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,32 @@
 <template>
-<div>dashboard</div>
+  <div class="row">
+    <div class="col-6">
+      <div class="card">
+        <div class="card-body">
+          <chart1></chart1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent, onMounted } from "vue";
-import { setCurrentPageTitle } from "@/core/helpers/breadcrumb";
-
+import { defineComponent, onMounted } from "vue"
+import { setCurrentPageTitle } from "@/core/helpers/breadcrumb"
+import chart1 from "@/components/dashboardcomp/chart1.vue"
 
 export default defineComponent({
   name: "dashboard",
+  components: {
+    chart1
+  },
   data() {
-    return {
-    };
+    return {}
   },
   setup() {
     onMounted(() => {
-      setCurrentPageTitle("Dashboard");
-    });
-  },
-});
+      setCurrentPageTitle("Dashboard")
+    })
+  }
+})
 </script>
