b6bea77955b8f79675e3593867df0a64954b7d86
oktaykaypak <36843265+oktaykaypak@users.noreply.github.com>
2021-09-30 00:49:49 +0300

    ödeme bekleyenler sayfası

--- src/views/pages/report/BoatOwnersPendingPayment.vue
+++ src/views/pages/report/BoatOwnersPendingPayment.vue
@@ -1,13 +1,120 @@
 <template>
-  <div> odeme bekleyen tekne sahipleri</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <pendingpaymentTable :list="list" type="comment" :tableTotalData="10" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import pendingpaymentTable from "@/components/reportpagecomp/pendingpaymentTable.vue";
 export default {
-
-}
+  components: { pendingpaymentTable },
+  data() {
+    return {
+      list: [
+        {
+          orderid: "56037-XDER",
+          country: {
+            name: "Brasil",
+            code: "PH",
+          },
+          date: {
+            value: "05/28/2020",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Intertico",
+            fields: "Web, UI/UX Design",
+          },
+          status: {
+            label: "Onaylandı",
+            color: "success",
+          },
+          total: "$3560",
+        },
+        {
+          orderid: "05822-FXSP",
+          country: {
+            name: "Belarus",
+            code: "BY",
+          },
+          date: {
+            value: "04/18/2021",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "Agoda",
+            fields: "Houses & Hotels",
+          },
+          status: {
+            label: "Beklemede",
+            color: "warning",
+          },
+          total: "$4850",
+        },
+        {
+          orderid: "4472-QREX",
+          country: {
+            name: "Phillipines",
+            code: "BH",
+          },
+          date: {
+            value: "07/23/2019",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "RoadGee",
+            fields: "Transportation",
+          },
+          status: {
+            label: "İptal",
+            color: "danger",
+          },
+          total: "$8376",
+        },
+        {
+          orderid: "00347-BCLQ",
+          country: {
+            name: "Argentina",
+            code: "BR",
+          },
+          date: {
+            value: "12/21/2021",
+            remarks: "Paid",
+          },
+          progress: {
+            value: "50",
+            color: "primary",
+          },
+          company: {
+            name: "The Hill",
+            fields: "Insurance",
+          },
+          status: {
+            label: "İade Talebi",
+            color: "info",
+          },
+          total: "$9486",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
